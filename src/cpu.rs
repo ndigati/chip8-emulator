ec269f7ca05dcb9efa1426aef2be7a9dba7cc7ce
Nick Digati <ndigati@gmail.com>
2015-12-23 21:41:52 -0800

    Started implementing CPU

--- src/cpu.rs
+++ src/cpu.rs
@@ -5,6 +5,7 @@ pub struct Cpu {
     // memory: Memory,
     registers: [u8; 16],
     pc: u16,
+    opcode: u8,
     I: u16,
     sp: u16,
     stack: [u16; 16],
@@ -14,14 +15,21 @@ impl Cpu {
     pub fn new() -> Cpu {
         Cpu {
             registers: [0; 16],
-            pc: 0,
+            pc: 0x200,              // Program counter starts at 0x200
+            opcode: 0,
             I: 0,
             sp: 0,
             stack: [0; 16],
         }
     }
 
-    pub fn print_stuff(&self) {
-        println!("{:?}", &self);
+    pub fn emulate_cycle() {
+        // Fetch opcode
+
+        // Decode opcode
+
+        // Execute opcode
+
+        // Update timers
     }
 }
