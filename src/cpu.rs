c7de3dc2028bd32a3522a5836be8b314fb63f569
Nick Digati <ndigati@gmail.com>
2015-12-27 16:11:01 -0800

    Added all opcodes and basic print statement for each.

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 // File to hold CPU functions and stuff
 use mem::Mem;
 
@@ -13,11 +15,11 @@ struct Registers {
     v7: u8,
     v8: u8,
     v9: u8,
-    vA: u8,
-    vB: u8,
-    vC: u8,
-    vD: u8,
-    vE: u8,     // used for the carry flag
+    va: u8,
+    vb: u8,
+    vc: u8,
+    vd: u8,
+    ve: u8,     // used for the carry flag
     pc: u16,
     i:  u16
 }
@@ -45,11 +47,11 @@ impl Cpu {
                 v7: 0,
                 v8: 0,
                 v9: 0,
-                vA: 0,
-                vB: 0,
-                vC: 0,
-                vD: 0,
-                vE: 0,
+                va: 0,
+                vb: 0,
+                vc: 0,
+                vd: 0,
+                ve: 0,
                 pc: 0x200,
                 i:  0,
             },
@@ -73,7 +75,64 @@ impl Cpu {
     
     fn decode_opcode(&self, opcode: u16) {
         match opcode & 0xF000 {
-            _ => println!("Doing nothing!")
+            0x0000  => {
+                // opcode's: 00E0, 00EE, or 0NNN
+                if opcode & 0x00F0 == 0x00E0 {
+                    println!("Clears the screen.");
+                } else if opcode & 0x00FF == 0x00EE {
+                    println!("Returns from a subroutine.");
+                } else {
+                    println!("Calls RCA 1802 program at address NNN. Not necessary for most ROMs.");
+                }
+            },
+            0x1000  => println!("Jumps to address NNN."),
+            0x2000  => println!("Calls subroutine at NNN."),
+            0x3000  => println!("Skips the next instruction if VX equals NN."),
+            0x4000  => println!("Skips the next instruction if VX doesn't equal NN."),
+            0x5000  => println!("Skips the next instruction if VX equals VY."),
+            0x6000  => println!("Sets VX to NN."),
+            0x7000  => println!("Adds NN to VX."),
+            0x8000  => {
+                match opcode & 0x000F {
+                    0x0000  => println!("Sets VX to the value of VY."),
+                    0x0001  => println!("Sets VX to VX or VY."),
+                    0x0002  => println!("Sets VX to VX and VY."),
+                    0x0003  => println!("Sets VX to VX xor VY."),
+                    0x0004  => println!("Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't."),
+                    0x0005  => println!("VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't."),
+                    0x0006  => println!("Shifts VX right by one. VF is set to the value of the least significant bit of VX before the shift."),
+                    0x0007  => println!("Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't."),
+                    0x000E  => println!("Shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift."),
+                    _       => println!("Non used opcode in the 0x8000 range")
+                }
+            },
+            0x9000  => println!("Skips the next instruction if VX doesn't equal VY."),
+            0xA000  => println!("Sets I to the address NNN."),
+            0xB000  => println!("Jumps to the address NNN plus V0."),
+            0xC000  => println!("Sets VX to the result of a bitwise and operation on a random number and NN."),
+            0xD000  => println!("Drawing stuff instruction"),
+            0xE000  => {
+                match opcode & 0x00FF {
+                    0x009E  => println!("Skips the next instruction if the key stored in VX is pressed."),
+                    0x00A1  => println!("Skips the next instruction if the key stored in VX isn't pressed."),
+                    _       => println!("Non used opcode in the 0xE000 range")
+                }
+            },
+            0xF000 => {
+                match opcode & 0x00FF {
+                    0x0007 => println!("Sets VX to the value of the delay timer."),
+                    0x000A => println!("A key press is awaited, and then stored in VX."),
+                    0x0015 => println!("Sets the delay timer to VX."),
+                    0x0018 => println!("Sets the sound timer to VX."),
+                    0x001E => println!("Adds VX to I."),
+                    0x0029 => println!("Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font."),
+                    0x0033 => println!("Stores the Binary-coded decimal representation of VX"),
+                    0x0055 => println!("Stores V0 to VX in memory starting at address I."),
+                    0x0065 => println!("Fills V0 to VX with values from memory starting at address I."),
+                    _       => println!("Unused opcode in the 0xF000 range")
+                }
+            },
+            _       => println!("Invalid/Unimplemented opcode")
         }
     }
 
